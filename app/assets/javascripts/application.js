8de2ae96d78c1eb8e3a11cbf97eb38bc7de532a2
willyam.zhang <willyam.zhang@innoark.com>
2019-08-14 22:15:22 +0700

    add gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require react
-//= require react_ujs
-//= require components
 //= require_tree .
-//= require jquery
-//= require bootstrap-sprockets
